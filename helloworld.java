a29438db39aacda798242e4f4240785bd32c57de
ga <2456760351@qq.com>
2024-07-01 11:53:07 +0800

    hello world

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+System.out.println("2");
+        System.out.println("Hello World1");
+
+System.out.println("Hello World3333333");
+    }
+}
